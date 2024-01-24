6096f871db2b9a76b081f0078b916c2be61c3bc5
EgorovEA <eaegorov42@gmail.com>
2024-01-25 02:00:22 +0500

    промежуточный вариант

--- project_kr3/main.py
+++ project_kr3/main.py
@@ -1,12 +1,7 @@
 import datetime
-from project_kr3.utils import(sort_data_by_datetime)
-# s1 = "2019-07-12T20:41:47.882230"
-# s2 = "2018-12-20T16:43:26.929246"
-# d1 = datetime.datetime.strptime(s1, "%Y-%m-%dT%H:%M:%S.%f").strftime('%d.%m.%Y')
-# d2 = datetime.datetime.strptime(s2, "%Y-%m-%dT%H:%M:%S.%f").strftime('%d.%m.%Y')
-#
-# print(d1)
-# print(d2)
+from project_kr3.utils import (sort_operations)
 
-list_data = sort_data_by_datetime()
-print(list_data)
+data_sort = sort_operations()
+for item in data_sort:
+    print(f"{item['date']} {item['description']}\n{item['from']} -> {item['to']}\n"
+          f"{item['operationAmount']['amount']} {item['operationAmount']['currency']['name']}\n")
--- project_kr3/utils.py
+++ project_kr3/utils.py
@@ -1,13 +1,14 @@
 import json
-from datetime import datetime
 
 
-# def sort_data_by_datetime():
-"""Функция сортровки json файла с данными"""
+def sort_operations():
+    """Функция сортирует json файл и возвращает последние пять операций"""
+    result_operations = []
+    with open("operations.json", "r", encoding='utf8') as json_file:
+        operations = json.load(json_file)
+        operations_sort = [item for item in operations if item.get('state') == "EXECUTED" and item.get('from') is not None]
+        operations_sort.sort(key=lambda x: x.get('date'), reverse=True)
+    for item in operations_sort[0:5]:
+        result_operations.append(item)
 
-    # list_data = []
-with open("operations.json", "r", encoding='utf8') as json_file:
-    data = json.loads()
-    data_list = sorted(data, key=lambda x:datetime.strptime(x["date"], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
-print(data)
-
+    return result_operations
