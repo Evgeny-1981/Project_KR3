b884eedf62ad39b7e1954d04a25c001c58279fef
EgorovEA <eaegorov42@gmail.com>
2024-01-26 07:07:18 +0500

    промежуточный вариант

--- project_kr3/main.py
+++ project_kr3/main.py
@@ -1,7 +1,9 @@
 import datetime
-from project_kr3.utils import (sort_operations)
+from project_kr3.utils import (sort_operations, return_last_operations)
 
-data_sort = sort_operations()
-for item in data_sort:
+
+operations_sort = sort_operations()
+last_operations = return_last_operations(operations_sort)
+for item in last_operations:
     print(f"{item['date']} {item['description']}\n{item['from']} -> {item['to']}\n"
           f"{item['operationAmount']['amount']} {item['operationAmount']['currency']['name']}\n")
--- project_kr3/utils.py
+++ project_kr3/utils.py
@@ -2,13 +2,29 @@ import json
 
 
 def sort_operations():
-    """Функция сортирует json файл и возвращает последние пять операций"""
-    result_operations = []
+    """Функция сортирует json файл"""
+
+
     with open("operations.json", "r", encoding='utf8') as json_file:
         operations = json.load(json_file)
-        operations_sort = [item for item in operations if item.get('state') == "EXECUTED" and item.get('from') is not None]
-        operations_sort.sort(key=lambda x: x.get('date'), reverse=True)
+    operations_sort = [item for item in operations if item.get('state') == "EXECUTED" and item.get('from') is not None]
+    operations_sort.sort(key=lambda x: x.get('date'), reverse=True)
+
+    return operations_sort
+
+
+def return_last_operations(operations_sort):
+    """Функция возвращает список последних 5 операций"""
+
+
+    result_operations = []
     for item in operations_sort[0:5]:
         result_operations.append(item)
 
     return result_operations
+
+def format_card_number():
+    pass
+
+def format_card_account():
+    pass
