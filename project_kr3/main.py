d88ecb7c2e7defc0317a826e87c6ec34b43726c0
EgorovEA <eaegorov42@gmail.com>
2024-01-26 16:03:46 +0500

    Реализовано с учетом отсутствия FROM

--- project_kr3/main.py
+++ project_kr3/main.py
@@ -6,8 +6,14 @@ from project_kr3.utils import (sort_operations, return_last_operations, format_p
 operations = sort_operations()
 last_operations = return_last_operations(operations)
 for item in last_operations:
-    print(f"{format_output_data(item['date'])} {item['description']}\n"
-          f"{format_private_num(item['from'])}"
-          f" -> {format_private_num(item['to'])}\n"
-          f"{item['operationAmount']['amount']}"
-          f" {item['operationAmount']['currency']['name']}\n")
+    if len(item) > 6:
+        print(f"{format_output_data(item['date'])} {item['description']}\n"
+              f"{format_private_num(item['from'])}"
+              f" -> {format_private_num(item['to'])}\n"
+              f"{item['operationAmount']['amount']}"
+              f" {item['operationAmount']['currency']['name']}\n")
+    else:
+        print(f"{format_output_data(item['date'])} {item['description']}\n"
+              f"{format_private_num(item['to'])}\n"
+              f"{item['operationAmount']['amount']}"
+              f" {item['operationAmount']['currency']['name']}\n")
