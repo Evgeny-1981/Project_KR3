f76f709913e70acde61c5b6ab9a0d5fa29f034e7
EgorovEA <eaegorov42@gmail.com>
2024-01-28 14:28:47 +0500

    tests

--- tests/test_project_kr3.py
+++ tests/test_project_kr3.py
@@ -5,20 +5,17 @@ from project_kr3.utils import (sort_operations, return_last_operations,
 
 
 def test_sort_operations():
-    pass
-
-
-@pytest.mark.parametrize("number, expected_result", [([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5])])
-def test_return_last_operations(number, expected_result):
-    assert return_last_operations(number) == expected_result
+    assert str(type(sort_operations())) == "<class 'list'>"
+def test_return_last_operations():
+    assert return_last_operations([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == [1, 2, 3, 4, 5]
 
 
 def test_format_output_date():
-    assert format_output_date("2019-08-26T10:50:58.294041") == "26.08.2019"
+    assert format_output_date("2024-01-28T10:50:58.294041") == "28.01.2024"
 
 
-@pytest.mark.parametrize("number, expected_result", [("1111111111111111", " 1111 11** **** 1111"),
-                                                     ("11111111111111111111", " **1111"),
-                                                     ("1", "Неверный или отсутствует номер/счет")])
+@pytest.mark.parametrize("number, expected_result", [("Maestro 1111111111111111", "Maestro 1111 11** **** 1111"),
+                                                     ("Счет 11111111111111111111", "Счет **1111"),
+                                                     ("11111", "Неверный или отсутствует номер/счет")])
 def test_format_account_number(number, expected_result):
     assert format_account_number(number) == expected_result
