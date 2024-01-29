49ddfae393b02dcdf674edbe8ed9e9ef2a34bee2
EgorovEA <eaegorov42@gmail.com>
2024-01-29 13:51:56 +0500

    version 0.3.2

--- tests/test_project_kr3.py
+++ tests/test_project_kr3.py
@@ -3,6 +3,7 @@ import project_kr3.utils
 
 
 def test_sort_operations():
+
     assert str(type(project_kr3.utils.sort_operations())) == "<class 'list'>"
 
 
@@ -14,8 +15,9 @@ def test_format_output_date():
     assert project_kr3.utils.format_output_date("2024-01-28T10:50:58.294041") == "28.01.2024"
 
 
-@pytest.mark.parametrize("number, expected_result", [("Maestro 1111111111111111", "Maestro 1111 11** **** 1111"),
-                                                     ("Счет 11111111111111111111", "Счет **1111"),
-                                                     ("11111", "Неверный или отсутствует номер/счет")])
-def test_format_account_number(number, expected_result):
-    assert project_kr3.utils.format_account_number(number) == expected_result
+@pytest.mark.parametrize("data, result", [("Maestro 1111111111111111", "Maestro 1111 11** **** 1111"),
+                                          ("Visa Classic 1111111111111111", "Visa Classic 1111 11** **** 1111"),
+                                          ("Счет 11111111111111111111", "Счет **1111"),
+                                          ("11111", "Неверный или отсутствует номер/счет")])
+def test_format_account_number(data, result):
+    assert project_kr3.utils.format_account_number(data) == result
